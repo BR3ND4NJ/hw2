97764e0419541fd98632900e703f2c9348a2661d
Brendan Johnson <biker8642@gmail.com>
2024-02-27 20:09:41 -0500

    finished 2.0

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ public class Main {
         // Example Usage
         RBStack<Integer> rbStack = new RBStack<>(10);
 
+        System.out.println("I'm going to push 1 into the red stack");
         rbStack.redPush(1);
+        System.out.println("I'm going to push 2 into the red stack");
         rbStack.redPush(2);
 
+        System.out.println("I'm going to push 10 into the blue stack");
         rbStack.bluePush(10);
+        System.out.println("I'm going to push 9 into the blue stack");
         rbStack.bluePush(9);
+        System.out.println();
 
-        System.out.println(rbStack.redPop());
-        System.out.println(rbStack.bluePop());
+        System.out.println("The top of the red stack is: " + rbStack.redPop());
+        System.out.println("The top of the blue stack is: " + rbStack.bluePop());
     }
 }
