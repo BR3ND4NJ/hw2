9c060a479fa184528f57bb955b7ea0eba0460df9
Brendan Johnson <biker8642@gmail.com>
2024-02-27 19:01:33 -0500

    first commit

---
+++ src/RBStack.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class RBStack {
+    public class ArrayStack<E> implements Stack<E> {
+        public static int CAPACITY = 1000;
+        private E[] data;
+        private int t = -1;
+        public ArrayStack(int capacity) {
+            data = (E[]) new Object[capacity];
+        }
+        public ArrayStack() {
+            this(CAPACITY);
+        }
+
+    }
+}
