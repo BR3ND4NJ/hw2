b52d5da8a3bc72c2136708b5b7311d90ac9a8b83
Brendan Johnson <biker8642@gmail.com>
2024-02-27 19:41:12 -0500

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Example Usage
+        RBStack<Integer> rbStack = new RBStack<>(10);
 
+        rbStack.redPush(1);
+        rbStack.redPush(2);
+
+        rbStack.bluePush(10);
+        rbStack.bluePush(9);
+
+        System.out.println(rbStack.redPop());
+        System.out.println(rbStack.bluePop());
     }
 }
--- src/RBStack.java
+++ src/RBStack.java
@@ -1,18 +1,83 @@
-import java.util.Stack;
+public class RBStack<E> {
+    private int capacity;
+    private E[] array;
+    private int redTop;
+    private int blueTop;
 
-public class RBStack {
-    public class ArrayStack<E> implements Stack<E> {
-        public static int CAPACITY = 1000;
-        private E[] data;
-        private int t = -1;
-        public ArrayStack(int capacity) {
-            data = (E[]) new Object[capacity];
+    public RBStack(int capacity) {
+        this.capacity = capacity;
+        this.array = (E[]) new Object[capacity];
+        this.redTop = -1;
+        this.blueTop = capacity;
+    }
+
+    public boolean isRedEmpty() {
+        return redTop == -1;
+    }
+
+    public boolean isBlueEmpty() {
+        return blueTop == capacity;
+    }
+
+    public boolean isFull() {
+        return redTop + 1 == blueTop;
+    }
+
+    public void redPush(E value) {
+        if (!isFull()) {
+            redTop++;
+            array[redTop] = value;
+        } else {
+            System.out.println("Red stack is full. Cannot push.");
         }
-        public ArrayStack() {
-            this(CAPACITY);
+    }
+
+    public void bluePush(E value) {
+        if (!isFull()) {
+            blueTop--;
+            array[blueTop] = value;
+        } else {
+            System.out.println("Blue stack is full. Cannot push.");
         }
+    }
 
-        //test 3
+    public E redPop() {
+        if (!isRedEmpty()) {
+            E value = array[redTop];
+            redTop--;
+            return value;
+        } else {
+            System.out.println("Red stack is empty. Cannot pop.");
+            return null;
+        }
+    }
 
+    public E bluePop() {
+        if (!isBlueEmpty()) {
+            E value = array[blueTop];
+            blueTop++;
+            return value;
+        } else {
+            System.out.println("Blue stack is empty. Cannot pop.");
+            return null;
+        }
+    }
+
+    public E redPeek() {
+        if (!isRedEmpty()) {
+            return array[redTop];
+        } else {
+            System.out.println("Red stack is empty. Cannot peek.");
+            return null;
+        }
+    }
+
+    public E bluePeek() {
+        if (!isBlueEmpty()) {
+            return array[blueTop];
+        } else {
+            System.out.println("Blue stack is empty. Cannot peek.");
+            return null;
+        }
     }
 }
